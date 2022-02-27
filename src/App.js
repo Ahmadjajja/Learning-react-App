2cf10c3862715795d8b75649b4c1a1cf87a2e22f
Ahmad jajja <ahmadjajja86@gmail.com>
2022-02-27 09:29:50 +0500

    updated first time

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+const  Header = () => {
+  return <h1>ahmad</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
